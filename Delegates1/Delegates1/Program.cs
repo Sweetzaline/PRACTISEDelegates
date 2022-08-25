06e4f5b60e5aad900356e750483ed260e7ed78b1
unknown <buragohain4@gmail.com>
2022-08-25 22:01:23 +0530

    Learning the concept of Delegate. Steps to cll a method using Delegate.

---
+++ Delegates1/Delegates1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delegates1
+{
+    public delegate void AddDelegate(int x, int y); //step1
+    public delegate string SayDelegate(string str);
+
+    class Program
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static string Say(string name)
+        {
+            return "Hello " + name + "!";
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learnig Delegate Concept!\n");
+
+            Program obj1 = new Program();
+            //obj1.Add(100, 20);//called the method by using the instance of the class
+           //Step2
+            AddDelegate ad = new AddDelegate(obj1.Add);//non static member is called in static method, so we need the instance of the class// Step 2
+            //Step3
+            ad(100, 200);
+            //ad.Invoke(100,200)//automatically generated, if not the upper statement
+            //invoke is the method inside the delegate, used to call the method.
+
+            //string obj2=Program.Say("Sweety");/called the method by using the name of the class
+
+
+            SayDelegate sd = new SayDelegate(Say);//static member can be directly accesed by the classname.
+            string str=sd("Sweety");
+            // string str= sd.Invoke("Sweety");
+
+            Console.WriteLine(str);
+        }
+    }
+}
