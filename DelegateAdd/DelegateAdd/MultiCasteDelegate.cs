3dc3313dc31c48c44d2383ed1577fa2273417ad6
unknown <buragohain4@gmail.com>
2022-08-28 01:35:15 +0530

    How To Add Delegate?

---
+++ DelegateAdd/DelegateAdd/MultiCasteDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace DelegateAdd
+{
+    public class MultiCasteDelegate
+    {
+        delegate void DelOp(int x, int y);
+        public static void ImplementDelegate()
+        {
+            //Delegate instantiation
+            DelOp obj = operation.Add;
+            obj += operation.Square;
+            obj(5, 5);
+            obj(3, 2);
+            Console.ReadLine();
+        }
+        public class operation
+        {
+            public static void Add(int a, int b)
+            {
+                Console.WriteLine("Addition={0} \n", a + b);
+            }
+            public static void Square(int a, int b)
+            {
+                Console.WriteLine("Multiple={0} \n", a * b);
+            }
+        }
+    }
+}
---
+++ DelegateAdd/DelegateAdd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DelegateAdd
+{
+    //Delegate Defination or Declaration
+    public delegate int operation(int x, int y);
+
+    class Program
+    {
+        //Method that passes as an Argument
+        //It has same signature as Delegates
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate POC!");
+            //Delegate instantiation
+            operation obj = new operation(Addition);
+            //output
+            Console.WriteLine("Addition is ={0} \n", obj(23, 24));
+            MultiCasteDelegate.ImplementDelegate();
+        }
+    }
+}
